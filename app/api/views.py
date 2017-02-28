e99aad20da2521858810e3b5558d75fdfc964036
Alexander Kalach <alexanderkalach@gmail.com>
2017-02-28 22:51:56 +0100

    Update docker files, add kafka-python

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from kafka import KafkaProducer
+
 
 def index(request):
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    for _ in range(100):
+        producer.send('test', b'some_message_bytes')
     return HttpResponse("Hey")
